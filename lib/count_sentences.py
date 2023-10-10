9e4d0430182d637017f226814f60888ef0b082ab
Moses-Oyelade <oyelademoses@gmail.com>
2023-10-10 01:02:03 +0100

    completed

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 
 class MyString:
-    def __init__(self):
-        self._is_sentence = ""
+    def __init__(self, value = "mystring"):
+        self.value = value
     
     
-    def is_sentence(self, value):
-        self._is_sentence == value
-        return value.endswith(".")
-    def is_question(self, value):
-        return value.endswith("?")
-    def is_exclamation(self, value):
-        return value.endswith("!")
-    def count_sentences(self, value):
+    def get_value(self):
+      return self.value
+    def set_value(self):
+      if type(self.value) in (str, float) or self.value == " ":
+        print("The value must be a string.")
+      else:
+        return self.value
+    
+    def is_sentence(self):
+        return self.value.endswith(".")
+    def is_question(self):
+        return self.value.endswith("?")
+    def is_exclamation(self):
+        return self.value.endswith("!")
+    def count_sentences(self):
       count = 0
-      for letter in value:
+      for letter in self.value:
         if letter == "." or letter == "?" or letter == "!": count += 1
       return count
               
@@ -24,5 +31,11 @@ class MyString:
 
 
 personal = MyString()
-print(personal.is_sentence("this is the town."))
-print(personal.count_sentences("This is a string! It has three sentences. Right?"))
+personal.value = "this is the town."
+print(personal.is_sentence())
+
+simple_string = MyString("one. teo. three?")
+empty_string = MyString()
+complex_string = MyString("This is a string! It has three sentences. Right?")
+
+print(simple_string.count_sentences())
